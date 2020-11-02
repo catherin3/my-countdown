d59e8ba3a93d11b7d23465b94be8cd32b3792687
Boogle Staff <melvintseng@BoogleStaff.local>
2020-11-02 16:09:53 +0800

    countdown timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [timerDays, setTimerDays] = useState('00');
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdowndate = new Date('November 9 2020 07:00:00').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdowndate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+      const mintues = Math.floor(distance % ((1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+      if (distance < 0) {
+        //stop the timer
+        clearInterval(interval.current)
+      } else {
+        //update the timer 
+        setTimerDays(days)
+        setTimerHours(hours)
+        setTimerMinutes(mintues)
+        setTimerSeconds(seconds)
+      }
+
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current)
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="timer">
+        <section>
+          <p>{timerDays}</p>
+          <p><small>Days</small></p>
+        </section>
+        <span>:</span>
+
+        <section>
+          <p>{timerHours}</p>
+          <p><small>Hours</small></p>
+        </section>
+        <span>:</span>
+
+        <section>
+          <p>{timerMinutes}</p>
+          <p><small>Minutes</small></p>
+        </section>
+        <span>:</span>
+
+        <section>
+          <p>{timerSeconds}</p>
+          <p><small>Seconds</small></p>
+        </section>
+      </section>
     </div>
   );
 }
